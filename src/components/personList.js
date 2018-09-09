604148b2ce9c83621f99d141f974d10f3516b128
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-09-09 15:01:05 -0400

    person list component

--- src/components/personList.js
+++ src/components/personList.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import Person from './person';
+import axios from 'axios';
+
+const config = {
+    headers : {
+		'Access-Control-Allow-Origin': '*',
+		Authorization: `${process.env.SL_KEY}`
+	}
+};
 
 class PersonList extends Component {
 
+	constructor(){
+        super();
+        this.state = {
+            people: [],
+        }
+    }
+
+	componentDidMount() {
+		axios.get('https://api.salesloft.com/v2/people.json', config)
+		.then(res => {
+			this.setState({
+				people: res.data.data
+			});
+		})
+	}
+		
+
 	render() {
 		return (
-            <div>
-                <Person />
+            <div className="card border-secondary mb-3">
+                {this.state.people.map(person => (
+					<div className="card-header" key={person.id}>
+						<p className="card-text">{person.first_name} {person.last_name}</p>
+						<div className="card-body text-secondary">
+							<p className="card-text">{person.email_address}</p>
+							<p className="card-text">{person.title}</p>
+						</div>
+					</div>
+				))}
             </div>
-			)
+		)
 	}
 }
 

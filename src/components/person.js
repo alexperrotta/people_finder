f46da1991cafe4a96d4990678663cbdc3e897a40
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-09-08 20:33:49 -0400

    person component

--- src/components/person.js
+++ src/components/person.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-
+import "./person.css";
 
 class Person extends Component {
 
 	render() {
 		return (
-            <div class="card border-secondary mb-3" style="max-width: 18rem;">
-                <div class="card-header">Name</div>
-                <div class="card-body text-secondary">
-                    <h5 class="card-title">Email</h5>
-                    <p class="card-text">Job Title</p>
+            <div className="card border-secondary mb-3">
+                <div className="card-header">Name</div>
+                <div className="card-body text-secondary">
+                    <p className="card-text">Email</p>
+                    <p className="card-text">Job Title</p>
                 </div>
             </div>
 			)

b01c9a63e49dde7d4aa7f394835e64b93709c6b8
Alex Perrotta <alexandra.perrotta@gmail.com>
2018-09-09 20:29:08 -0400

    added comments to PersonList comp

--- src/components/personList.js
+++ src/components/personList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import "./personList.css";
 
+// added api key to the .env file
+// assigned the api key to a variable so the key is not exposed publicly
 const config = {
     headers : {
 		'Access-Control-Allow-Origin': '*',
@@ -18,6 +20,7 @@ class PersonList extends Component {
         }
     }
 
+	// retrieving the people data from the api
 	componentDidMount() {
 		axios.get('https://api.salesloft.com/v2/people.json', config)
 		.then(res => {
@@ -32,6 +35,7 @@ class PersonList extends Component {
 		return (
             <div className='container'>
 				<div className="card border-secondary mb-3">
+					{/* iterate through each person to get their info and render to the screen */}
 					{this.state.people.map(person => (
 						<div className="card-header" key={person.id}>
 							<p className="card-text">{person.first_name} {person.last_name}</p>
